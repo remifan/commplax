e2ad4a6d6b1689c1eb43ed0365e24d12177ac5e7
Remi Qirui FAN <fanqirui318@gmail.com>
2021-04-11 17:24:48 +0800

    add devputlike helpful function

--- commplax/util.py
+++ commplax/util.py
@@ -1,12 +1,17 @@
 import jax
 
+def devputlike(x, y):
+    '''put x into the same device with y'''
+    return jax.device_put(x, y.device_buffer.device())
+
+
 def gpuexists():
     gpus = jax.devices('gpu')
     return len(gpus) != 0
 
 def gpufirstbackend():
     '''
-    `backend` api is experimental feature,
+    NOTE: `backend` api is experimental feature,
     https://jax.readthedocs.io/en/latest/jax.html#jax.jit
     '''
     return 'gpu' if gpuexists() else 'cpu'
