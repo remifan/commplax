98bbb86ecf081cc276fdb5c09276235f3137bb15
Remi Qirui FAN <fanqirui318@gmail.com>
2021-04-20 23:52:48 +0800

    add momentum

--- commplax/cxopt.py
+++ commplax/cxopt.py
@@ -139,10 +139,35 @@ def sgd(step_size):
   def init(x0):
     return x0
   def update(i, g, x):
-    return x - step_size(i) * jnp.conj(g)
+    return x - step_size(i) * g.conj()
   def get_params(x):
     return x
   return Optimizer(init, update, get_params)
+
+
+@optimizer
+def momentum(step_size: Schedule, mass: float):
+  """Construct optimizer triple for SGD with momentum.
+  Args:
+    step_size: positive scalar, or a callable representing a step size schedule
+      that maps the iteration index to positive scalar.
+    mass: positive scalar representing the momentum coefficient.
+  Returns:
+    An (init_fun, update_fun, get_params) triple.
+  """
+  step_size = make_schedule(step_size)
+  def init(x0):
+    v0 = jnp.zeros_like(x0)
+    return x0, v0
+  def update(i, g, state):
+    x, velocity = state
+    velocity = mass * velocity + g.conj()
+    x = x - step_size(i) * velocity
+    return x, velocity
+  def get_params(state):
+    x, _ = state
+    return x
+  return init, update, get_params
 
 
 @optimizer
