d23e3256e6953d56e20854f6eb621ca915b2be7c
Remi Qirui FAN <fanqirui318@gmail.com>
2021-04-26 19:31:54 +0800

    improve readability, use util.tree_update to handle params update

--- commplax/dspmodel.py
+++ commplax/dspmodel.py
@@ -1,21 +1,68 @@
 import jax
+from jax.api import value_and_grad
 import numpy as np
 from jax import numpy as jnp, jit, vmap
 from functools import partial
 from collections import namedtuple
-from commplax import op, xop, comm, xcomm, adaptive_filter as af, equalizer as eq
+from typing import NamedTuple, Any, Optional, Union, Callable
+from commplax import op, xop, comm, xcomm, adaptive_filter as af, equalizer as eq, util
 
 
-LMSHparams = namedtuple('LMSHparams', ['taps', 'lr_w', 'lr_f', 'lr_b', 'lockgain'], defaults=(31, 1/2**6, 1/2**7, 1/2**11, False))
-DBPHparams = namedtuple('DBPHparams', ['sr', 'lspan', 'nspan', 'taps', 'xi', 'lpdbm', 'stps', 'vspan', 'fc', 'disp', 'ftaps'],
-                        defaults=(None, None, None, None, 0.5, 30., 1, None, 299792458/1550E-9, 16.5E-6, 1))
-MFHparams = namedtuple('MFHparams', ['taps', 'kind'], defaults=(129, 'delta'))
-DBPParams = namedtuple('DBPParams', ['d', 'n'])
-DSPModel = namedtuple('DSPModel', ['params', 'inpbuf', 'afstat', 'delay', 'eziter', 'iter', 'model', 'value_and_grad'])
+class LMSHparams(NamedTuple):
+    taps: int = 31
+    lr_w: float = 1/2**6
+    lr_f: float = 1/2**7
+    lr_b: float = 1/2**11
+    lockgain: bool = False
 
 
-def matchedfilter(y, h, mode='same'):
-  return vmap(lambda y, h: xop.convolve(y, h, mode=mode), in_axes=-1, out_axes=-1)(y, h)
+class DBPHparams(NamedTuple):
+    sr: Optional[float] = None
+    lspan: Optional[int] = None
+    nspan: Optional[int] = None
+    dtaps: Optional[int] = None
+    vspan: Optional[int] = None
+    xi: float = .5
+    lpdbm: float = 30.
+    stps: int = 1
+    fc: float = 299792458/1550E-9
+    disp: float = 16.5E-6
+    ntaps: int=1
+
+
+class MFHparams(NamedTuple):
+    taps: int = 129
+    kind: str = 'delta'
+
+
+class DBPParams(NamedTuple):
+    d: Optional[Union[jnp.ndarray, np.ndarray]] = None
+    n: Optional[Union[jnp.ndarray, np.ndarray]] = None
+
+
+class DBPLMSParams(NamedTuple):
+    mf: Optional[Union[jnp.ndarray, np.ndarray]] = None
+    dbp: DBPParams = DBPParams()
+
+
+DBPLMSInpBuf = namedtuple('DBPLMSInpBuf', ['ybuf', 'xbuf', 'fomulbuf'])
+DBPLMSAFStat = namedtuple('DBPLMSAFStat', ['mimo'])
+DBPLMSAFVals = namedtuple('DBPLMSAFVals', ['mimo'])
+
+
+class DSPModel(NamedTuple):
+    params: DBPLMSParams
+    inpbuf: DBPLMSInpBuf
+    afstat: DBPLMSAFStat
+    delay: int
+    eziter: Callable
+    iterate: Callable
+    model: Callable
+    value_and_grad: Callable
+
+
+def matchedfilter(y, h, mode='valid'):
+    return vmap(lambda y, h: xop.convolve(y, h, mode=mode), in_axes=-1, out_axes=-1)(y, h)
 
 
 def delta(taps, dims=2, dtype=np.complex64):
@@ -24,74 +71,71 @@ def delta(taps, dims=2, dtype=np.complex64):
     return mf
 
 
-def dbplms(var=None,
-           sps=2,
-           dims=2,
-           modformat='16QAM',
-           dbphparams: DBPHparams=None,
-           mfhparams: MFHparams=MFHparams(taps=129, kind='delta'),
-           lmshparams: LMSHparams=(LMSHparams(taps=31, lr_w=1/2**6, lr_f=1/2**7, lr_b=1/2**11, lockgain=False))):
+def dbpinit(hparams: DBPHparams, value: DBPParams=DBPParams(), dims=2):
+    if value is None or value.d is None or value.n is None:
+        _, paramD, paramN = comm.dbp_params(hparams.sr,
+                                            hparams.lspan,
+                                            hparams.nspan,
+                                            hparams.dtaps,
+                                            launch_power=hparams.lpdbm,
+                                            steps_per_span=hparams.stps,
+                                            virtual_spans=hparams.vspan,
+                                            fiber_dispersion=hparams.disp)
+        if hparams.ntaps > 1:
+            # extend to mimo filter
+            paramN = np.stack([delta(hparams.ntaps, dims=dims * dims, dtype=np.float64).reshape((hparams.ntaps, dims, dims))] \
+                              * paramN.shape[0]) * paramN[:, None, :, :]
+        value = DBPParams(d=paramD, n=paramN * hparams.xi)
+
+    steps = value.d.shape[0]
+    dtaps = value.d.shape[1]
+    ntaps = value.n.shape[1] if value.n.ndim > 3 else 1
+    taps = steps * (dtaps - 1 + ntaps - 1) + 1 # equivalent linear filter taps
+    valid = af.filterzerodelaypads(taps)[0]
+    return value, valid
+
+
+def mfinit(hparams: MFHparams, value=None, dims=2):
+    if value is None:
+        if hparams.kind.lower() == 'delta':
+            value = delta(hparams.taps, dims)
+        else:
+            raise ValueError('matched filter of the specified kind is not support yet')
+
+    taps = value.shape[0]
+    valid = af.filterzerodelaypads(taps)[0]
+    return value, valid
+
+
+def dbplms(var: DBPLMSParams=DBPLMSParams(),
+           sps: int=2,
+           dims: int=2,
+           modformat: str='16QAM',
+           dbphparams: DBPHparams=DBPHparams(),
+           mfhparams: MFHparams=MFHparams(),
+           lmshparams: LMSHparams=(LMSHparams())):
+
     qamscale = xcomm.qamscale(modformat)
 
     lms_init, lms_update, lms_map = af.ddlms(lr_w=lmshparams.lr_w,
                                              lr_f=lmshparams.lr_f,
                                              lr_b=lmshparams.lr_b,
                                              lockgain=lmshparams.lockgain)
-    mimotaps = lmshparams.taps
+    lms_vld = af.filterzerodelaypads(lmshparams.taps, stride=sps)[0]
+    dbp, dbp_vld = dbpinit(dbphparams, value=var.dbp)
+    mf, mf_vld = mfinit(mfhparams, value=var.mf)
 
-    try:
-        mf=var.mf
-    except AttributeError:
-        if mfhparams.kind.lower() == 'delta':
-            mf = delta(mfhparams.taps)
-        else:
-            raise ValueError('matched filter of the specified kind is not support yet')
-
-    if dbphparams is not None:
-        try:
-            dbp=var.dbp
-        except AttributeError:
-            _, paramD, paramN = comm.dbp_params(dbphparams.sr,
-                                                dbphparams.lspan,
-                                                dbphparams.nspan,
-                                                dbphparams.taps,
-                                                launch_power=dbphparams.lpdbm,
-                                                steps_per_span=dbphparams.stps,
-                                                virtual_spans=dbphparams.vspan,
-                                                fiber_dispersion=dbphparams.disp)
-            if dbphparams.ftaps > 1:
-                paramN = np.stack([delta(dbphparams.ftaps, dims=dims * dims, dtype=np.float64).reshape((dbphparams.ftaps, dims, dims))] \
-                                  * paramN.shape[0]) * paramN[:, None, :, :]
-            dbp = DBPParams(d=paramD, n=paramN * dbphparams.xi)
-    else:
-        raise ValueError('dbp and cdc hparams cannot be both absent')
-
-    mftaps = mf.shape[0]
-    dtaps = dbp.d.shape[1]
-    ftaps = dbp.n.shape[1]
-    steps = dbp.d.shape[0]
-    dbptaps = steps * (dtaps - 1 + ftaps - 1) + 1 # equivalent linear filter taps
-
-    cdcpads = af.filterzerodelaypads(dbptaps)
-    mfpads = af.filterzerodelaypads(mftaps)
-    mimopads = af.filterzerodelaypads(mimotaps, stride=sps)
-    ybuf = jnp.zeros(((cdcpads[0] + mfpads[0] + mimopads[0]).sum(), dims), dtype=np.complex64)
-    xbuf = jnp.zeros(((cdcpads[0, 0] + mfpads[0, 0] + mimopads[0, 0]) // sps, dims), dtype=np.complex64)
-    fobuf = jnp.ones((ybuf.shape[0] - cdcpads[0, 0], 1), dtype=np.complex64)
-    mimostat = lms_init(mimotaps, mimoinit='centralspike')
+    ybuf = jnp.zeros(((dbp_vld + mf_vld + lms_vld).sum(), dims), dtype=np.complex64)
+    xbuf = jnp.zeros(((dbp_vld[1] + mf_vld[1] + lms_vld[1]) // sps, dims), dtype=np.complex64)
+    fobuf = jnp.ones((ybuf.shape[0], 1), dtype=np.complex64)
     dspdelay = xbuf.shape[0]
 
-    Params = namedtuple('Params', ['mf', 'dbp'])
-    InpBuf = namedtuple('InpBuf', ['ybuf', 'xbuf', 'fomulbuf'])
-    AFStat = namedtuple('AFStat', ['mimo'])
-    AFVals = namedtuple('AFVals', ['mimo'])
-
-    params0 = Params(mf, dbp)
-    inpbuf0 = InpBuf(ybuf, xbuf, fobuf)
-    afstate0 = AFStat(mimostat)
+    params0 = DBPLMSParams(mf, dbp)
+    inpbuf0 = DBPLMSInpBuf(ybuf, xbuf, fobuf)
+    afstate0 = DBPLMSAFStat(lms_init(lmshparams.taps, mimoinit='centralspike'))
 
     @jit
-    def updinpbuf(inp, inpbuf: InpBuf):
+    def streaminputs(inp, inpbuf: DBPLMSInpBuf):
         y, x, fomul = inp
         ybuf, xbuf, fobuf, = inpbuf
         y = jnp.concatenate([ybuf, y])
@@ -101,14 +145,16 @@ def dbplms(var=None,
         x = x[:-xbuf.shape[0]]
         fomul = jnp.concatenate([fobuf, fomul])
         fobuf = fomul[-fobuf.shape[0]:]
-        fomul = fomul[:-cdcpads[0, 1]]
-        return (y, x, fomul), InpBuf(ybuf, xbuf, fobuf)
+        fomul = fomul[dbp_vld[0]:-dbp_vld[1]]
+        return (y, x, fomul), DBPLMSInpBuf(ybuf, xbuf, fobuf)
 
     def tddbp(y, d, n, conv=xop.fftconvolve):
         return xcomm.dbp_timedomain(y, d, n, mode='valid', homosteps=True, scansteps=True, conv=conv)
 
-    def iterate(inp, params: Params, inpbuf: InpBuf, afstate: AFStat, conv=xop.fftconvolve):
-        inp, inpbuf = updinpbuf(inp, inpbuf)
+    def iterate(inp: Any, params: DBPLMSParams, inpbuf: DBPLMSInpBuf, afstate: DBPLMSAFStat, conv=xop.fftconvolve):
+        inp, inpbuf = streaminputs(inp, inpbuf)
+
+        params = util.tree_update(params0, params)
 
         y, x, fomul = inp
         mf, dbp = params
@@ -121,12 +167,12 @@ def dbplms(var=None,
         mimostat, (mimovals, _) = af.iterate(lms_update, mimostat, zf, x)
         z = lms_map(mimovals, zf)
 
-        afstate = AFStat(mimostat,)
-        afvals = AFVals(mimovals,)
+        afstate = DBPLMSAFStat(mimostat,)
+        afvals = DBPLMSAFVals(mimovals,)
         return z, inpbuf, afstate, afvals
 
-    def model(inp, params: Params, inpbuf: InpBuf, afvals: AFVals, conv=xop.fftconvolve):
-        inp, inpbuf = updinpbuf(inp, inpbuf)
+    def model(inp: Any, params: DBPLMSParams, inpbuf: DBPLMSInpBuf, afvals: DBPLMSAFVals, conv=xop.fftconvolve):
+        inp, inpbuf = streaminputs(inp, inpbuf)
 
         y, _, fomul = inp
         mf, dbp = params
@@ -139,16 +185,16 @@ def dbplms(var=None,
         z = lms_map(mimovals, zf)
         return z, inpbuf
 
-    def lossfn(v, z, inp, params: Params, inpbuf: InpBuf, afvals: AFVals):
-        zhat, _ = model(inp, params._replace(**v._asdict()), inpbuf, afvals)
+    def lossfn(v: DBPLMSParams, z: Any, inp: Any, params: DBPLMSParams, inpbuf: DBPLMSInpBuf, afvals: DBPLMSAFVals):
+        zhat, _ = model(inp, util.tree_update(params, v), inpbuf, afvals)
         return jnp.mean(jnp.abs(zhat - z)**2)
 
     @jit
-    def value_and_grad(v, z, inp, params: Params, inpbuf: InpBuf, afvals: AFVals):
+    def value_and_grad(v: DBPLMSParams, z: Any, inp: Any, params: DBPLMSParams, inpbuf: DBPLMSInpBuf, afvals: DBPLMSAFVals):
         loss, grads = jax.value_and_grad(lossfn)(v, z, inp, params, inpbuf, afvals)
         return loss, grads
 
-    def eziter(inp, params: Params, zerodelayroll=True, backend='cpu', conv=xop.conv1d_fft_oa):
+    def eziter(inp: Any, params: DBPLMSParams, zerodelayroll=True, backend='cpu', conv=xop.conv1d_fft_oa):
         z = jit(partial(iterate, conv=conv), backend=backend)(inp, params, inpbuf0, afstate0)[0]
         return xop.delay(z, -dspdelay) if zerodelayroll else z
 
--- commplax/util.py
+++ commplax/util.py
@@ -1,8 +1,16 @@
 import jax
+from jax._src.util import partial, safe_zip, safe_map, unzip2
+from jax import tree_util
+from jax.tree_util import (tree_map, tree_flatten, tree_unflatten,
+                           register_pytree_node)
+
+map = safe_map
+zip = safe_zip
 
 
 def getdev(x):
     return x.device_buffer.device()
+
 
 def devputlike(x, y):
     '''put x into the same device with y'''
@@ -21,3 +29,32 @@ def gpufirstbackend():
     '''
     return 'gpu' if gpuexists() else 'cpu'
 
+
+def tree_like(x, value=None):
+    x_flat, x_tree = tree_flatten(x)
+    v_flat = (value,) * len(x_flat)
+    return tree_unflatten(x_tree, v_flat)
+
+
+def tree_update_ignorenoneleaves(x, y):
+    '''
+    replace x's leaves with y's non-None leaves
+    '''
+    x_flat, x_tree = tree_flatten(x)
+    # WORKAROUND to make tree_flatten recognize None-valued pytree nodes as pytree leaves,
+    # so that we can update (1, 2, (3, 4)) by (None, None, (5, None)) without errors
+    # see https://fossies.org/linux/tensorflow/tensorflow/compiler/xla/python/pytree.cc
+    # note returned y_tree also see None type as * type now
+    # not sure if filter `is_leaf=lambda n: not isinstance(n, tuple)` has side effects, be cautious!
+    y_flat, y_tree = tree_flatten(y, is_leaf=lambda n: not isinstance(n, tuple))
+
+    if x_tree != y_tree:
+      msg = ("tree update function produced an output structure that "
+             "did not match its input structure: input {} and output {}.")
+      raise TypeError(msg.format(x_tree, y_tree))
+    z_flat = map(lambda a, b: a if b is None else b, x_flat, y_flat)
+    z_tree = tree_unflatten(x_tree, z_flat)
+    return z_tree
+
+
+tree_update = tree_update_ignorenoneleaves
