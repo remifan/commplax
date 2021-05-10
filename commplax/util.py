781f597543cf790496474f93c0c965a8508a485b
Remi Qirui FAN <fanqirui318@gmail.com>
2021-05-11 00:04:31 +0800

    add flatten utilities

--- commplax/util.py
+++ commplax/util.py
@@ -62,3 +62,44 @@ def tree_update_ignorenoneleaves(x, y):
 
 
 tree_update = tree_update_ignorenoneleaves
+
+
+def isnamedtupleinstance(x):
+    _type = type(x)
+    bases = _type.__bases__
+    if len(bases) != 1 or bases[0] != tuple:
+        return False
+    fields = getattr(_type, '_fields', None)
+    if not isinstance(fields, tuple):
+        return False
+    return all(type(i)==str for i in fields)
+
+
+def unpack_namedtuple(obj):
+    if isinstance(obj, dict):
+        return {key: unpack_namedtuple(value) for key, value in obj.items()}
+    elif isinstance(obj, list):
+        return [unpack_namedtuple(value) for value in obj]
+    elif isnamedtupleinstance(obj):
+        return {key: unpack_namedtuple(value) for key, value in obj._asdict().items() if not (isinstance(value, tuple) and len(value) == 0)}
+    elif isinstance(obj, tuple):
+        return tuple(unpack_namedtuple(value) for value in obj)
+    else:
+        return obj
+
+
+def dict_flatten(d, parent_key='', sep='_'):
+    try:
+        from collections.abc import MutableMapping
+    except ModuleNotFoundError:
+        from collections import MutableMapping
+    items = []
+    for k, v in d.items():
+        new_key = parent_key + sep + k if parent_key else k
+        if isinstance(v, MutableMapping):
+            items.extend(dict_flatten(v, new_key, sep=sep).items())
+        else:
+            items.append((new_key, v))
+    return dict(items)
+
+
