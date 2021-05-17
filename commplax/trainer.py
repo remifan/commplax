57774626d7934dfde3878e69ba232ece98ff3dff
Remi Qirui FAN <fanqirui318@gmail.com>
2021-05-17 23:14:59 +0800

    skip training if there are no trainable weights

--- commplax/trainer.py
+++ commplax/trainer.py
@@ -39,20 +39,21 @@ tmap = TrainableMap(all=_all,
                     only_freeze=_only(_freeze))
 
 
-def train(model: cl.Layer,
-          batch_generator,
-          loss_metric: cl.Layer=cl.MSE(),
-          input_shape=(None, 2),
-          init_weights=None,
-          trainable=tmap.train_all,
-          optimizer=cxopt.adam(1e-5),
-          freeze_weights=False,
-          rng=random.PRNGKey(0),
-          s_batch=500,
-          n_iter=None,
-          n_epoch=1,
-          jit_backend='cpu',
-          state_schedule=lambda i, s0, s: s):
+def supervised(model: cl.Layer,
+               batch_generator,
+               loss_metric: cl.Layer=cl.MSE(),
+               input_shape=(None, 2),
+               init_weights=None,
+               trainable=tmap.train_all,
+               optimizer=cxopt.adam(1e-5),
+               freeze_weights=False,
+               rng=random.PRNGKey(0),
+               s_batch=500,
+               n_iter=None,
+               n_epoch=1,
+               jit_backend='cpu',
+               state_schedule=lambda i, s0, s: s):
+
     assert isinstance(model, cl.Layer), "invalid model"
     assert isinstance(loss_metric, cl.Layer), "invalid loss"
 
@@ -68,40 +69,43 @@ def train(model: cl.Layer,
     opt_state = opt_init(weights)
     w_trainable = trainable(util.tree_full(weights, True))
 
-    batch_num = 0
-    batch_gens = []
-    for _ in range(n_epoch):
-        batch_num, batch_gen = batch_generator(model_loss, s_batch)
-        batch_gens.append(batch_gen)
-    assert batch_num > 0, "empty batch"
-
-    n_iter = n_epoch * batch_num if n_iter is None else n_iter
-    freeze_weights = cxopt.make_schedule(freeze_weights)
-
-    @partial(jit, backend=jit_backend)
-    def step(i, opt_state: cxopt.OptimizerState, model_state, batch):
-        inputs, targets, aux = (*batch, ())[:3]
-        kwargs = {} if len(aux) == 0 else aux._asdict()
-        weights = get_params(opt_state)
-        (loss, model_state), g = cl.value_and_grad(model_loss)(weights, inputs, model_state, truth=targets, **kwargs)
-        opt_state = opt_update(i, apply_trainable_grad(g, w_trainable, not freeze_weights(i)), opt_state)
-        return loss, g, model_state, opt_state
-
-    loss = None
-    grad = None
-    i_iter = 0
-    model_state = model_state0
     TrainResult = namedtuple('TrainResult', 'weights aux')
     AuxOut = namedtuple('AuxOut', 'loss grad state')
-    for i_epoch in range(n_epoch):
-        model_state = state_schedule(i_epoch, model_state0, model_state)
-        for _ in trange(min(n_iter, batch_num), desc='epoch %d/%d' % (i_epoch, n_epoch), leave=False):
-            loss, grad, model_state, opt_state = step(i_iter,
-                                                      opt_state,
-                                                      model_state,
-                                                      next(batch_gens[i_epoch]))
-            i_iter += 1
-            n_iter -= 1
-            yield TrainResult(get_params(opt_state), AuxOut(float(loss), grad, model_state))
 
+    if util.tree_any(w_trainable):
+        batch_num = 0
+        batch_gens = []
+        for _ in range(n_epoch):
+            batch_num, batch_gen = batch_generator(model_loss, s_batch)
+            batch_gens.append(batch_gen)
+        assert batch_num > 0, "empty batch"
+
+        n_iter = n_epoch * batch_num if n_iter is None else n_iter
+        freeze_weights = cxopt.make_schedule(freeze_weights)
+
+        @partial(jit, backend=jit_backend)
+        def step(i, opt_state: cxopt.OptimizerState, model_state, batch):
+            inputs, targets, aux = (*batch, ())[:3]
+            kwargs = {} if len(aux) == 0 else aux._asdict()
+            weights = get_params(opt_state)
+            (loss, model_state), g = cl.value_and_grad(model_loss)(weights, inputs, model_state, truth=targets, **kwargs)
+            opt_state = opt_update(i, apply_trainable_grad(g, w_trainable, not freeze_weights(i)), opt_state)
+            return loss, g, model_state, opt_state
+
+        loss = None
+        grad = None
+        i_iter = 0
+        model_state = model_state0
+        for i_epoch in range(n_epoch):
+            model_state = state_schedule(i_epoch, model_state0, model_state)
+            for _ in trange(min(n_iter, batch_num), desc='epoch %d/%d' % (i_epoch, n_epoch), leave=True):
+                loss, grad, model_state, opt_state = step(i_iter,
+                                                          opt_state,
+                                                          model_state,
+                                                          next(batch_gens[i_epoch]))
+                i_iter += 1
+                n_iter -= 1
+                yield TrainResult(get_params(opt_state), AuxOut(float(loss), grad, model_state))
+    else:
+        yield TrainResult(get_params(opt_state), AuxOut(None, None, None))
 
