4bab531208f1116a0f7ed5c546b1cfbf155b6257
Remi Qirui FAN <fanqirui318@gmail.com>
2023-11-16 14:11:38 +0800

    add freqs check in testing dm

--- tests/dm_test.py
+++ tests/dm_test.py
@@ -6,6 +6,13 @@ from jax._src import test_util as jtu
 
 from commplax import dist_matcher as dm
 
+def count_freqs(syms, M):
+    freqs_out = {}
+    for i in range(M):
+        freqs_out[i] = 0
+    for s in syms:
+        freqs_out[s] += 1
+    return freqs_out
 
 class DMTest(jtu.JaxTestCase):
     @jtu.sample_product(
@@ -24,11 +31,18 @@ class DMTest(jtu.JaxTestCase):
         key = jr.key(s)
         bits_enc = jr.randint(key, (rate[0],), 0, 2)
 
+        # check encode and decode
         syms = encode(bits_enc)
         bits_dec = decode(syms)
 
         self.assertAllClose(bits_enc, bits_dec)
-        #TODO check freqs
+
+        # validate encoded freqs
+        freqs_in = {k: v for k, v in enumerate(freqs)}
+        freqs_out = count_freqs(np.array(syms), M)
+
+        self.assertTrue(freqs_in == freqs_out)
+
 
 if __name__ == "__main__":
     absltest.main(testLoader=jtu.JaxTestLoader())
