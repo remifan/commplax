4ca34d40ee959d3d3ee4d4f1cbf9c2c130decf7a
Remi QR FAN <remi.qr.fan@gmail.com>
2024-02-21 14:47:24 +0800

    add git into devcontainer

--- .devcontainer/install-dev-tools.sh
+++ .devcontainer/install-dev-tools.sh
@@ -2,7 +2,7 @@
 apt-get update
 apt-get upgrade -y
 # install Linux tools and Python 3
-apt-get install software-properties-common wget curl \
+apt-get install software-properties-common git wget curl \
     python3-dev python3-pip python3-wheel python3-setuptools -y
 # install Python packages
 python3 -m pip install --upgrade pip
