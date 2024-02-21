53917ab9773b5aaa2d1e76d7720eced2bef73fbf
Remi QR FAN <remi.qr.fan@gmail.com>
2024-02-21 16:55:11 +0800

    update devcontainer

--- .devcontainer/install-dev-tools.sh
+++ .devcontainer/install-dev-tools.sh
@@ -1,6 +1,7 @@
 # update system
 apt-get update
 apt-get upgrade -y
+DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends tzdata
 # install Linux tools and Python 3
 apt-get install software-properties-common git wget curl \
     python3-dev python3-pip python3-wheel python3-setuptools -y
