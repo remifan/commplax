e25ecfd1423386692fd8a766c985d5a08fd3e3ea
Remi QR FAN <remi.qr.fan@gmail.com>
2024-02-22 22:55:51 +0800

    migrate devcontainer to prebuild

--- .devcontainer/install-dev-tools.sh
+++ .devcontainer/install-dev-tools.sh
@@ -1,20 +1,3 @@
-# update system
-apt-get update
-DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends tzdata
-
-# install Linux tools and Python 3
-apt-get install software-properties-common git wget curl \
-    python3-dev python3-pip python3-wheel python3-setuptools -y
-
 # install Python packages
 python3 -m pip install --upgrade pip
-pip3 install --user -r .devcontainer/requirements.txt
-
-# install recommended packages
-apt-get install zlib1g g++ freeglut3-dev \
-    libx11-dev libxmu-dev libxi-dev libglu1-mesa libglu1-mesa-dev libfreeimage-dev -y
-
-# clean up
-pip3 cache purge
-apt-get autoremove -y
-apt-get clean
+pip3 install --no-cache-dir --user -r .devcontainer/requirements.txt
