0d297dfe2582a754b62c274d4fd74c669fab3756
Remi QR FAN <remi.qr.fan@gmail.com>
2025-07-08 18:55:53 +0800

    add MAP-BCJR detector

--- commplax/detector.py
+++ commplax/detector.py
@@ -17,7 +17,7 @@ import numpy as np
 import jax
 import matplotlib.pyplot as plt
 from functools import cache
-from jax import numpy as jnp
+from jax import numpy as jnp, scipy as jsp
 from functools import partial
 
 
@@ -27,7 +27,7 @@ def _unpack(d, B, N):
                         jnp.arange(N), unroll=True)[1].T[..., ::-1]
 
 
-def viterbi(I, L, const, btd=None):
+def mlse_viterbi(I, L, const, btd=None):
     # I: input size (e.g. 2 for binary)
     # L: memory
     # btd: backtrace depth
@@ -45,14 +45,13 @@ def viterbi(I, L, const, btd=None):
     pm_0 = jnp.zeros(S)
     tr_s_0 = jnp.zeros((T, S), dtype=int)
     tr_x_0 = jnp.zeros((T, S), dtype=int)
+    state_0 = (pm_0, tr_s_0, tr_x_0)
 
-    def step(carry, inp):
-        pm_k, tr_s, tr_x = carry
-        y_k, w_k = inp
+    def step(state, y_k):
+        pm_k, tr_s, tr_x, v_k = state
 
-        v_t = jnp.inner(u, w_k)      # shape: (S, I); can be factored out when w_k is static
-        bm_t = jnp.abs(v_t - y_k)**2
-        pm_tmp = pm_k[ps] + bm_t
+        bm_k = jnp.abs(v_k - y_k)**2  # branch metirc
+        pm_tmp = pm_k[ps] + bm_k
         idx = jnp.argmin(pm_tmp, axis=1)
         pm_k = pm_tmp[si, idx]
 
@@ -65,20 +64,60 @@ def viterbi(I, L, const, btd=None):
                          jnp.arange(T-1, -1, -1),
                          unroll=True)[1][-1] # complete unroll for the benefit of GPU
 
-        carry = (pm_k, tr_s, tr_x)
-        out = x
+        state = pm_k, tr_s, tr_x, v_k
+        out = const[x]
 
-        return carry, out
+        return state, out
 
     @jax.jit
-    def scan(ys, h, pm_0=pm_0, tr_s=tr_s_0, tr_x=tr_x_0):
-        # TODO: handle time varying h
-        K = ys.shape[0]
+    def scan(state, ys, h, pm_0=pm_0, tr_s=tr_s_0, tr_x=tr_x_0):
         w = jnp.pad(h, [0, L+1-h.shape[0]])
-        ws = jnp.repeat(w[None, :], K, axis=0)
-        carry = (pm_0, tr_s_0, tr_x_0)
-        carry, x_hat = jax.lax.scan(step, carry, (ys, ws))
-        return carry, x_hat
+        v = jnp.inner(u, w)
+        carry = state + (v,)
+        carry, x_hat = jax.lax.scan(step, carry, ys)
+        state = carry[:3]
+        return state, x_hat
 
-    return scan
+    return scan, state_0
+
+
+def map_bcjr(y, I, L, const, priori=None):
+    # I: input size
+    # L: memory
+    # N: traceback length
+    S = I**L # states number
+    si = jnp.arange(S) # state indices
+    ii = jnp.arange(I) # input indices
+    priori = jnp.ones(I)/I if priori is None else priori 
+    # ps[s] contains all the state (indices) yielding to state s with input (indices) pi[s].
+    ps = jnp.tile(si.reshape((-1, I)), (I, 1)) # shape: (S, I)
+    pi = jnp.repeat(jnp.repeat(ii[:, None], I, axis=1), S//I, axis=0) # shape: (S, I)
+    # u[s] contains all the states (values) yielding to state s with input (indices) pi[s].
+    u = jax.vmap(lambda s: const[_unpack(ps[s] + pi[s] * S, I, L+1)])(si) # shape: (S, I, L+1)
+ 
+    lse = jsp.special.logsumexp
+ 
+    noise_std = 0.001
+    w = jnp.pad(h, [0, L+1-h.shape[0]])
+    v = jnp.inner(u, w)
+ 
+    err = jnp.abs(y[:, None, None] - v)**2
+    log_priori = jnp.log(priori + 1e-30)[pi]
+    gamma = -err / (2 * noise_std**2) - jnp.log(np.sqrt(2 * np.pi) * noise_std) + log_priori
+    alpha = jax.lax.scan(
+        lambda a, g: (lse((a[ps] + g), axis=1), a),
+        jnp.full(S, 0), gamma,
+    )[1]
+    beta = jax.lax.scan(
+        lambda b, g: (lse((b[:, None] + g).reshape((I, I * S//I)), axis=0), b),
+        jnp.full(S, 0), gamma[::-1, ...],
+    )[1][::-1, ...]
+
+    lp = (P:=lse((alpha[:,ps] + gamma + beta[..., None]).reshape((-1, I, S)), axis=2)) \
+        - lse(P, axis=1)[:, None] # log normalization
+    xi = jnp.argmax(lp, axis=1)
+    x_hat = const[xi]
+    llr = lp[:, :, None] - lp[:, None, :]
+        
+    return x_hat, llr
 
